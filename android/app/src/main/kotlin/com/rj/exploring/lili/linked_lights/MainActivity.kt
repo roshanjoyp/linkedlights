f7b8ae4ae21cc7b5f334190924a42c07f4d5caab
Roshan Joy <roshanjoyp@gmail.com>
2025-02-22 13:00:09 +0530

    initial commit with stacked

---
+++ android/app/src/main/kotlin/com/rj/exploring/lili/linked_lights/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.rj.exploring.lili.linked_lights
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app/app.dart
@@ -0,0 +1,30 @@
+import 'package:linked_lights/ui/bottom_sheets/notice/notice_sheet.dart';
+import 'package:linked_lights/ui/dialogs/info_alert/info_alert_dialog.dart';
+import 'package:linked_lights/ui/views/home/home_view.dart';
+import 'package:linked_lights/ui/views/startup/startup_view.dart';
+import 'package:stacked/stacked_annotations.dart';
+import 'package:stacked_services/stacked_services.dart';
+// @stacked-import
+
+@StackedApp(
+  routes: [
+    MaterialRoute(page: HomeView),
+    MaterialRoute(page: StartupView),
+    // @stacked-route
+  ],
+  dependencies: [
+    LazySingleton(classType: BottomSheetService),
+    LazySingleton(classType: DialogService),
+    LazySingleton(classType: NavigationService),
+    // @stacked-service
+  ],
+  bottomsheets: [
+    StackedBottomsheet(classType: NoticeSheet),
+    // @stacked-bottom-sheet
+  ],
+  dialogs: [
+    StackedDialog(classType: InfoAlertDialog),
+    // @stacked-dialog
+  ],
+)
+class App {}
